f171b988e313bfcb9eff3e171529f6b7c702ca3a
rahalrazika <rahalraika@gmail.com>
2020-12-17 18:41:23 +0100

    add contact section

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import header from './modules/navbar';
 import containre from './modules/home';
 import about from './modules/about'
+import contact from './modules/contact'
 
 const content = document.querySelector('.content');
 
 content.appendChild(header);
 content.appendChild(containre);
 content.appendChild(about);
+content.appendChild(contact);
---
+++ src/modules/contact.js
@@ -0,0 +1,29 @@
+import bg from '../images/bg.jpg';
+const contact = document.createElement('div');
+contact.classList.add('contact')
+const text = document.createElement('h1')
+text.classList.add('text-white', 'mt-5', 'contact-text', 'pb4')
+text.textContent = `Contacte US `
+const phoneText = document.createElement('h2')
+phoneText.classList.add('text-white', 'contact-text')
+phoneText.textContent = `Phone :`
+
+const phoneNumber = document.createElement('h2')
+phoneNumber.classList.add('text-white', 'contact-text')
+phoneNumber.textContent = `0049 - 123 456 789`
+
+const adressText = document.createElement('h2')
+adressText.classList.add('text-white', 'contact-text')
+adressText.textContent = `Adress :`
+
+const adress = document.createElement('h2')
+adress.classList.add('text-white', 'contact-text')
+adress.textContent = `Gtrand St. New York`
+
+contact.appendChild(text)
+contact.appendChild(phoneText)
+contact.appendChild(phoneNumber)
+contact.appendChild(adressText)
+contact.appendChild(adress)
+
+export default contact;
